24b85b558b61da48d5891afdbf3c2501170a267e
fertinaYolanda27 <sprinkleseason69@gmail.com>
2025-11-18 07:57:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+ * File JavaScript untuk Portofolio Fertina Yolanda
+ *
+ * Saat ini masih kosong.
+ *
+ * Rencana di masa depan:
+ * 1. Menambahkan fungsionalitas untuk menu navigasi mobile (hamburger menu).
+ * 2. Menambahkan animasi "fade-in" saat section di-scroll.
+ */
+
+console.log("Portofolio script loaded.");
